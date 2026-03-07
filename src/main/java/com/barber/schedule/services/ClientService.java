9c22eca40d90f83f59f933492e851929b5329687
Arthur Barbosa <arthurbarbosaabab261@gmail.com>
2026-03-07 00:21:14 -0300

    Created insert, delete and update methods

--- src/main/java/com/barber/schedule/services/ClientService.java
+++ src/main/java/com/barber/schedule/services/ClientService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -21,6 +22,29 @@ public class ClientService {
     public Client findById(Long id){
         Optional<Client> obj = clientRepository.findById(id);
         return obj.get();
+    }
+
+    public Client insert(Client obj){
+        return clientRepository.save(obj);
+    }
+
+    public void delete(Long id){
+        clientRepository.deleteById(id);
+    }
+
+    public Client update(Long id, Client obj){
+        Client entity = clientRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return clientRepository.save(entity);
+    }
+
+    public void updateData(Client entity, Client obj){
+        if(obj.getName() != null) {
+            entity.setName(obj.getName());
+        }
+        if(obj.getPhone() != null) {
+            entity.setPhone(obj.getPhone());
+        }
     }
 
 
