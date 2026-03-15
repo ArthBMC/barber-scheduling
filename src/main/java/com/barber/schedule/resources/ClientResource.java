2949ce96c13d6022b188c3c55a345d3e45392bef
Arthur Barbosa <arthurbarbosaabab261@gmail.com>
2026-03-15 20:52:19 -0300

    cleaning imports

--- src/main/java/com/barber/schedule/resources/ClientResource.java
+++ src/main/java/com/barber/schedule/resources/ClientResource.java
@@ -4,10 +4,11 @@ import com.barber.schedule.entities.Client;
 import com.barber.schedule.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,19 +28,6 @@ public class ClientResource {
     public ResponseEntity<Client> findById(@PathVariable Long id){
         Client obj = clientService.findById(id);
         return ResponseEntity.ok().body(obj);
-    }
-
-    @GetMapping(value = "/")
-    public ResponseEntity<Client> findByPhone(@RequestParam String phone) {
-        Client client = clientService.findByPhone(phone);
-        return ResponseEntity.ok().body(client);
-    }
-
-    @PostMapping
-    public ResponseEntity<Client> insert(@RequestBody Client obj){
-        obj = clientService.insert(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.created(uri).body(obj);
     }
 
 }
